00b890bc56110b513c89e12092c48170b6f303d6
Данил <157314066+Selyakov661@users.noreply.github.com>
2024-01-24 01:16:59 +0700

    Delete Laba7 directory

--- Laba7/Django/mysite/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.get_name, name='user_name'),
-    path('user/search', views.user_by_name, name='user_by_name'),
-]
